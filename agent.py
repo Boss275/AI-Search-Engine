7bc9fe5a88dd272af7cbfe065068c229ad07524a
Boss275 <135417106+Boss275@users.noreply.github.com>
2025-09-21 22:13:01 +0500

    agent.py

--- agent.py
+++ agent.py
@@ -1,4 +1,4 @@
-'''import os
+import os
 from huggingface_hub import InferenceClient
 import wikipedia
 import arxiv
@@ -40,29 +40,4 @@ def agent_run(prompt: str) -> str:
     except Exception as e:
         wiki = wiki_search(prompt)
         arx = arxiv_search(prompt)
-        return f"Model error: {e}\n\nWikipedia:\n{wiki}\n\narXiv:\n{arx}"'''
-
-
-
-
-import os
-from langchain.llms import HuggingFaceHub
-from langchain.agents import initialize_agent, load_tools
-
-HF_TOKEN = os.getenv("HF_TOKEN")
-HF_MODEL = os.getenv("HF_MODEL", "meta-llama/Llama-3.1-8B-Instruct")
-
-llm = HuggingFaceHub(
-    repo_id=HF_MODEL,
-    model_kwargs={"temperature": 0, "max_new_tokens": 500},
-    huggingfacehub_api_token=HF_TOKEN
-)
-
-tools = load_tools(["wikipedia", "arxiv"], llm=llm)
-
-agent = initialize_agent(
-    tools=tools,
-    llm=llm,
-    agent="zero-shot-react-description",
-    verbose=True
-)
+        return f"Model error: {e}\n\nWikipedia:\n{wiki}\n\narXiv:\n{arx}"
