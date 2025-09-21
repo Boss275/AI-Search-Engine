7d56aeed31572126f3ba2060e2343d768acc5cd6
Boss275 <135417106+Boss275@users.noreply.github.com>
2025-09-21 17:16:21 +0500

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 from agent import agent
 
 OPENAI_API_KEY = "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"
-PROJECT_ID = "	proj_QHsK4ZJE2mnu6bkcuEGYAGyI"
+PROJECT_ID = "proj_QHsK4ZJE2mnu6bkcuEGYAGyI"
 ORG_ID = "org-V8sG6TuRjJhPJa6MTlZHURN0"
 client = OpenAI(api_key=OPENAI_API_KEY, project=PROJECT_ID, organization=ORG_ID)
 
